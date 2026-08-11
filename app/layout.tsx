c4d3e198d8e46c32152debf01c58014027f3e7d2
Darko Mitrev <darkomitrev@Darkos-MacBook-Air.local>
2026-08-11 19:39:27 +0200

    Fix ClashDisplay-Semibold filename casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const clashDisplay = localFont({
     {path: "./fonts/ClashDisplay-Light.otf", weight: "300", style:"normal"},
     {path: "./fonts/ClashDisplay-Regular.otf", weight: "400", style: "normal"},
     {path: "./fonts/ClashDisplay-Medium.otf", weight: "500", style: "normal"},
-    {path: "./fonts/ClashDisplay-SemiBold.otf", weight: "600", style: "normal"},
+    {path: "./fonts/ClashDisplay-Semibold.otf", weight: "600", style: "normal"},
     {path: "./fonts/ClashDisplay-Bold.otf", weight: "700", style:"normal"},
   ],
   variable: "--font-clash",
